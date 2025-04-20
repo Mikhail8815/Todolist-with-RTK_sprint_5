5b7b2a16604b77d1bf806f41b344410c0bbc4563
Mikhail8815 <mishutka150588@mail.ru>
2025-04-20 22:19:55 +0300

    first commit

---
+++ src/features/auth/model/auth-slice.ts
@@ -0,0 +1,97 @@
+import { createAppSlice, handleServerAppError, handleServerNetworkError } from "@/common/utils"
+import { setAppStatusAC } from "@/app/app-slice.ts"
+import { Inputs } from "@/features/auth/lib/schemas"
+import { authApi } from "@/features/auth/api/authApi.ts"
+import { ResultCode } from "@/common/enums"
+import { AUTH_TOKEN } from "@/common/constants/constants.ts"
+import { clearDataAC } from "@/common/actions"
+
+export const authSlice = createAppSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+  },
+  selectors: {
+    selectIsLoggedIn: (state) => state.isLoggedIn,
+  },
+  reducers: (create) => ({
+    loginTC: create.asyncThunk(
+      async (data: Inputs, { dispatch, rejectWithValue }) => {
+        try {
+          dispatch(setAppStatusAC({ status: "loading" }))
+          const res = await authApi.login(data)
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(setAppStatusAC({ status: "succeeded" }))
+            localStorage.setItem(AUTH_TOKEN, res.data.data.token)
+            return { isLoggedIn: true }
+          } else {
+            handleServerAppError(res.data, dispatch)
+            return rejectWithValue(null)
+          }
+        } catch (error: any) {
+          handleServerNetworkError(error, dispatch)
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+          // state[action.payload.todolistId] = action.payload.tasks
+        },
+      },
+    ),
+    logoutTC: create.asyncThunk(
+      async (_, { dispatch, rejectWithValue }) => {
+        try {
+          dispatch(setAppStatusAC({ status: "loading" }))
+          const res = await authApi.logout()
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(setAppStatusAC({ status: "succeeded" }))
+            dispatch(clearDataAC())
+            localStorage.removeItem(AUTH_TOKEN)
+            return { isLoggedIn: false }
+          } else {
+            handleServerAppError(res.data, dispatch)
+            return rejectWithValue(null)
+          }
+        } catch (error: any) {
+          handleServerNetworkError(error, dispatch)
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+          // state[action.payload.todolistId] = action.payload.tasks
+        },
+      },
+    ),
+    initializeAppTC: create.asyncThunk(
+      async (_, { dispatch, rejectWithValue }) => {
+        try {
+          dispatch(setAppStatusAC({ status: "loading" }))
+          const res = await authApi.me()
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(setAppStatusAC({ status: "succeeded" }))
+            return { isLoggedIn: true }
+          } else {
+            handleServerAppError(res.data, dispatch)
+            return rejectWithValue(null)
+          }
+        } catch (error: any) {
+          handleServerNetworkError(error, dispatch)
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state.isLoggedIn = action.payload.isLoggedIn
+        },
+      },
+    ),
+  }),
+})
+
+export const { selectIsLoggedIn } = authSlice.selectors
+export const { loginTC, logoutTC, initializeAppTC } = authSlice.actions
+export const authReducer = authSlice.reducer
