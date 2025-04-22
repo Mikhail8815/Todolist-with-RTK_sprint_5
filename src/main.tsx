c467fe53cb959d95ebfd6a6c78b6892711c86f55
Mikhail8815 <mishutka150588@mail.ru>
2025-04-22 23:11:19 +0300

    Login, logout, Me (3)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { App } from "@/app/App"
 import { createRoot } from "react-dom/client"
-import "./index.css"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router"
 import { store } from "./app/store"
